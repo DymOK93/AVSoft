ffd41c1413de38c8834a923c169a410c289cff22
DymOK <dym93ok@yandex.ru>
2020-08-22 23:29:04 +0300

    .gitignore update

--- settings.h
+++ settings.h
@@ -2,5 +2,5 @@
 #define READERS_COUNT 3
 #define WRITERS_COUNT 1
 #define READER_DELAY 50	/*milliseconds*/
-#define WRITER_DELAY 100	/*milliseconds*/
-#define ITERATIONS_COUNT 10	/**/
+#define WRITER_DELAY 100 /*milliseconds*/
+#define ITERATIONS_COUNT 10	
